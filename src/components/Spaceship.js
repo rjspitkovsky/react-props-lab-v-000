2ceb9d1110fe20faf077f8d3fa2fc66d366593fc
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-30 18:45:14 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -15,7 +15,7 @@ export class Spaceship extends React.Component {
 }
 
 Spaceship.defaultProps = {
-  speed: "slow"
-  hasRockets: false 
+  speed: "slow",
+  hasRockets: false,
   colors: ["black, red"]
 }
